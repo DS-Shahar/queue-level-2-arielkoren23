a7a8e84dc066e60371a434cabd15af3b0c632fe1
arielkoren23 <arielkoren23@gmail.com>
2026-01-04 00:13:50 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,91 @@
-class Main {
-  public static void main(String[] args) {
+class Main 
+{
+  public static void main(String[] args) 
+  {
     System.out.println("Hello World!");
   }
+  
+  public static Queue<Integer> runLengths(Queue<Character> q)
+  {
+    Queue<Integer> res = new Queue<>();
+    if (q.isEmpty())
+        return res;
+
+    char ch = q.remove();
+    int count = 1;
+    while (!q.isEmpty())
+    {
+        char x = q.remove();
+
+        if (x == ch)
+        {
+            count++;
+        }
+        else
+        {
+            res.insert(count);
+            ch = x;
+            count = 1;
+        }
+    }
+    res.insert(count);
+    return res;
+  }
+
+  public static Queue<Integer> mergeQueues(Queue<Integer> q1, Queue<Integer> q2)
+{
+    Queue<Integer> res = new Queue<>();
+
+    while (!q1.isEmpty() && !q2.isEmpty())
+    {
+        if (q1.head() <= q2.head())
+            res.insert(q1.remove());
+        else
+            res.insert(q2.remove());
+    }
+
+    while (!q1.isEmpty())
+        res.insert(q1.remove());
+
+    while (!q2.isEmpty())
+        res.insert(q2.remove());
+
+    return res;
+}
+
+  public static int sumLongestEvenRun(Queue<Integer> q)
+{
+    int maxLen = 0;
+    int maxSum = 0;
+
+    int len = 0;
+    int sum = 0;
+
+    while (!q.isEmpty())
+    {
+        int x = q.remove();
+
+        if (x % 2 == 0)
+        {
+            len++;
+            sum += x;
+        }
+        else
+        {
+            if (len > maxLen)
+            {
+                maxLen = len;
+                maxSum = sum;
+            }
+            len = 0;
+            sum = 0;
+        }
+    }
+
+    if (len > maxLen)
+        maxSum = sum;
+
+    return maxSum;
+}
+  
 }
